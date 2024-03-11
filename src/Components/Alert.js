46dfec8b315f2aac5f7eef33057c770ccbb24087
princepadit <prince123841426@gmail.com>
2024-03-11 10:44:07 +0530

    cm

--- src/Components/Alert.js
+++ src/Components/Alert.js
@@ -1,15 +1,15 @@
 import React from 'react'
 
 export default function Alert(props) {
-    const capitalize =(word)=>{
+    const capitalize=(word)=>{
         const lower =word.toLowerCase();
         return lower.charAt(0).toUpperCase()+ lower.slice(1);
     }
   return (
     <div style={{height: '50px'}}>
    { props.alert && <div className={`alert alert-${props.alert.type} alert-dismissible fade show`} role="alert">
-       <strong>  {props.alert.type}</strong>:{props.alert.msg}
-          {/* <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button> */}
+       <strong>  {capitalize(props.alert.type)}</strong>:{props.alert.msg}
+          
       </div>}
       </div>
     
